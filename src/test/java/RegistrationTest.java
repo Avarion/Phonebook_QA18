56c994c7a2fc2b94b375d58b393d623e02052ea3
Avarion <bogoborec87@gmail.com>
2023-03-06 08:05:39 +0200

    HW_8

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -2,7 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,16 +12,14 @@ public class RegistrationTest {
 
 
     @BeforeMethod
-    public void init()
-
-    {
+    public void init() {
         wd = new ChromeDriver();
         wd.navigate().to("https://telranedu.web.app/home");
     }
 
 
     @Test
-    public void regPositiveTest(){
+    public void regPositiveTest() {
 
         /*
         1. open
@@ -46,17 +43,51 @@ public class RegistrationTest {
         passInput.sendKeys("Illumiel1!");
 
         wd.findElement(By.xpath("//button[2]")).click();
+    }
+//      Assert.assertTrue(wd.findElement(By.xpath("//a[text()='ADD']")).getText().equals("ADD"));
 
-        Assert.assertTrue(wd.findElement(By.xpath("//a[text()='ADD']")).getText().equals("ADD"));
+    @Test
+    public void regNegativeTestMail() {
+        WebElement loginBtn1 = wd.findElement(By.xpath("//a[@href='/login']"));
+        loginBtn1.click();
 
+//          int i = (int) (System.currentTimeMillis() / 1000) % 3600;
+        WebElement emailInput1 = wd.findElement(By.xpath("//input[1]"));
+        emailInput1.click();
+        emailInput1.clear();
+        emailInput1.sendKeys("avarion87gmail.com");
 
+        WebElement passInput1 = wd.findElement(By.xpath("//input[2]"));
+        passInput1.click();
+        passInput1.clear();
+        passInput1.sendKeys("Illumiel1!");
+
+        wd.findElement(By.xpath("//button[2]")).click();
+    }
+
+    @Test
+    public void regNegativeTestPass() {
+        WebElement loginBtn1 = wd.findElement(By.xpath("//a[@href='/login']"));
+        loginBtn1.click();
+
+//      int i = (int) (System.currentTimeMillis() / 1000) % 3600;
+        WebElement emailInput1 = wd.findElement(By.xpath("//input[1]"));
+        emailInput1.click();
+        emailInput1.clear();
+        emailInput1.sendKeys("avarion87@gmail.com");
+
+        WebElement passInput1 = wd.findElement(By.xpath("//input[2]"));
+        passInput1.click();
+        passInput1.clear();
+        passInput1.sendKeys("Illumiel");
+
+        wd.findElement(By.xpath("//button[2]")).click();
     }
 
 
     @AfterMethod
-    public void tearDown()
-    {
-       // wd.quit();
+    public void tearDown() {
+        // wd.quit();
     }
 
 
